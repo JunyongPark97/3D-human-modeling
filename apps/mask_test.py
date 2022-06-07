f70915665e4d5bc942a183f53bc745812f8d92e6
JunyongPark97 <park@DESKTOP-62S7UKI.localdomain>
2022-06-08 01:39:17 +0900

    [UPDATED] Download path

--- apps/mask_test.py
+++ apps/mask_test.py
@@ -5,15 +5,15 @@ from PIL import Image
 from skimage.segmentation import clear_border
 import skimage.morphology as mp
 import scipy.ndimage.morphology as sm
-image = Image.open('/home/ubuntu/Downloads/test_0.png')
+image = Image.open('/home/ubuntu/Downloads/3dmodel/test_0.png')
 image = image.resize((512,512))
-image.save('/home/ubuntu/Downloads/test_0.png')
+image.save('/home/ubuntu/Downloads/3dmodel/test_0.png')
 
-image = Image.open('/home/ubuntu/Downloads/test_0.png')
+image = Image.open('/home/ubuntu/Downloads/3dmodel/test_0.png')
 model_cut = np.asarray(image)
 b = (model_cut == 0)
 c = b.astype(int)
 c[c != 1] = 255
 c[c == 1] = 0
 
-cv2.imwrite('/home/ubuntu/Downloads/test_0_mask.png', c)
+cv2.imwrite('/home/ubuntu/Downloads/3dmodel/test_0_mask.png', c)
